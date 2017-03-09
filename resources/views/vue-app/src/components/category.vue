4af669d5570c848d5438a3778238fe85392f0002
王小明 <symenywong@163.com>
2017-03-09 11:56:52 +0800

    11

--- resources/views/vue-app/src/components/category.vue
+++ resources/views/vue-app/src/components/category.vue
@@ -2,72 +2,99 @@
   <div class="template-content">
     <div class="bar bar-header">
         <search v-model="search_input" position="static" top="0"  class="list-search"></search>
-        <tab active-color='#fc378c' v-model="index">
+        <tab active-color='#FB4F5B' v-model="index">
           <tab-item>分类</tab-item>
           <tab-item>场景</tab-item>
           <tab-item>范围</tab-item>
         </tab>
     </div>
     <div class="category content">
-      <swiper :show-dots="false" class="category-swiper padding-t-10" v-model="index">
-        <swiper-item>
-          <tree-list></tree-list>
-        </swiper-item>
-        <swiper-item>
-          <ul class="category-scene">
-            <li v-for="(item,index) in scene">
-            <router-link :to="item.url" class="link-img">
-              <img :src="item.img" alt="">
-            </router-link>
-            </li>
-          </ul>
-        </swiper-item>
-        <swiper-item>
-          <group class="range-content">
-            <flexbox wrap="nowrap" :gutter="0">
-              <flexbox-item :span="3" class="text-center font-normal">
-                频段范围
-                <p class="font-mini">(MHZ)</p>
-              </flexbox-item>
-              <flexbox-item :span="4">
-                <x-input title="" type="text" keyboard="number" v-model="range_1">
-                </x-input>
-              </flexbox-item>
-              <flexbox-item :span="1" class="text-center">
-                <span class="iconfont">&#xe64b;</span>
-              </flexbox-item>
-              <flexbox-item :span="4">
-                <x-input title="" type="text" keyboard="number" v-model="range_2">
-                </x-input>
-              </flexbox-item>
-            </flexbox>
-          </group>
-          <group class="range-content">
-            <flexbox wrap="nowrap" :gutter="0">
-              <flexbox-item :span="3" class="text-center font-normal">
-                覆盖范围
-                <p class="font-mini">(㎡)</p>
-              </flexbox-item>
-              <flexbox-item :span="4">
-                <x-input title="" type="text" keyboard="number" v-model="area1">
-                </x-input>
-              </flexbox-item>
-              <flexbox-item :span="1" class="text-center">
-                <span class="iconfont">&#xe64b;</span>
-              </flexbox-item>
-              <flexbox-item :span="4">
-                <x-input title="" type="text" keyboard="number" v-model="area2">
-                </x-input>
-              </flexbox-item>
-            </flexbox>
-          </group>
-          <div class="tab-80 block-center margin-tb-20">
-            <x-button class="font-1x">
-              搜索
-            </x-button>
-          </div>
-        </swiper-item>
-      </swiper>
+      <div class="tab-item-content" v-if="index==0">
+        <flexbox class="tree-box" wrap="nowrap" :gutter="0">
+          <flexbox-item :span="3" class="tree-box-left">
+            <div class="padding-tb-6 padding-rl-10" :class="{'border-1px-b':index==(category_list.length-1),'node-active':index==node_index}" v-for="(item,index) in category_list" @click="handle_folder(index)">
+              {{item.name}}
+            </div>
+          </flexbox-item>
+          <flexbox-item :span="9" class="tree-box-right padding-l-6 border-box">
+            <div class="node-box" v-if="node_index==0">
+              <div class="node-title padding-tb-6 color-gray border-1px-b">
+                {{choose_node.name}}
+                <div class="pull-right color-danger" @click="clear_history">清除记录 <icon type="cancel"></icon></div>
+              </div>
+              <div class="node-content" v-for="child in choose_node.kwds">
+                <x-button mini class="pull-left margin-rl-6 margin-tb-4">
+                    {{child}}
+                </x-button>
+              </div>
+            </div>
+            <div class="node-box" v-if="node_index!=0" v-for="child in choose_node.children">
+              <div class="node-title border-1px-b padding-b-10 padding-tb-4">
+                {{child.name}}
+              </div>
+              <div class="node-content clear-float">
+                <div class="margin-tb-4 margin-rl-6 pull-left" v-for="_item in child.children">
+                  <x-button mini>{{_item.name}}</x-button>
+                </div>
+              </div>
+            </div>
+          </flexbox-item>
+        </flexbox>
+      </div>
+      <div class="tab-item-content" v-if="index==1">
+        <ul class="category-scene">
+          <li v-for="(item,index) in scene">
+          <router-link :to="item.url" class="link-img">
+            <img :src="item.img" alt="">
+          </router-link>
+          </li>
+        </ul>
+      </div>
+      <div class="tab-item-content" v-if="index==2">
+        <group class="range-content">
+          <flexbox wrap="nowrap" :gutter="0">
+            <flexbox-item :span="3" class="text-center font-normal">
+              频段范围
+              <p class="font-mini">(MHZ)</p>
+            </flexbox-item>
+            <flexbox-item :span="4">
+              <x-input title="" type="text" keyboard="number" v-model="range_1">
+              </x-input>
+            </flexbox-item>
+            <flexbox-item :span="1" class="text-center">
+              <span class="iconfont">&#xe64b;</span>
+            </flexbox-item>
+            <flexbox-item :span="4">
+              <x-input title="" type="text" keyboard="number" v-model="range_2">
+              </x-input>
+            </flexbox-item>
+          </flexbox>
+        </group>
+        <group class="range-content">
+          <flexbox wrap="nowrap" :gutter="0">
+            <flexbox-item :span="3" class="text-center font-normal">
+              覆盖范围
+              <p class="font-mini">(㎡)</p>
+            </flexbox-item>
+            <flexbox-item :span="4">
+              <x-input title="" type="text" keyboard="number" v-model="area1">
+              </x-input>
+            </flexbox-item>
+            <flexbox-item :span="1" class="text-center">
+              <span class="iconfont">&#xe64b;</span>
+            </flexbox-item>
+            <flexbox-item :span="4">
+              <x-input title="" type="text" keyboard="number" v-model="area2">
+              </x-input>
+            </flexbox-item>
+          </flexbox>
+        </group>
+        <div class="tab-80 block-center margin-tb-20">
+          <x-button class="font-1x">
+            搜索
+          </x-button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -75,13 +102,326 @@
 <script>
 import TreeList from './tree-list'
 //import SidePanel from './side-panel'
-import {Tab,TabItem,Search,Sticky,Swiper,SwiperItem,Group,Cell,XInput,Flexbox,FlexboxItem,XButton} from 'vux'
+import {Tab,TabItem,Search,Sticky,Swiper,SwiperItem,Group,Cell,XInput,Flexbox,FlexboxItem,XButton,Icon} from 'vux'
 export default {
   name:"category",
   data:function(){
     return {
       search_input:'',
       index:0,
+      node_index:0,
+      choose_node:{},
+      category_list:[
+        {
+          name:"搜索记录",
+          title:'history',
+          kwds:['室内天线','手机','无线基站','室外设备','耗材',"50-100",'天线','手机','基站','无线设备','耗材'],
+          children:[]
+        },
+        {
+          name:"子类1",
+          children:[
+            {
+              name:"子类1-1",
+              children:[
+                {
+                  name:'子类1-1-1',
+                  children:[]
+                },
+                {
+                  name:'子类1-1-2',
+                  children:[]
+                },
+                {
+                  name:'子类1-1-3',
+                  children:[]
+                }
+              ]
+            },{
+              name:"子类1-2",
+              children:[]
+            },{
+              name:"子类1-3",
+              children:[]
+            },{
+              name:"子类1-4",
+              children:[]
+            },{
+              name:"子类1-5",
+              children:[]
+            }
+          ],
+        },{
+          name:"子类2",
+          children:[
+            {
+              name:"子类2-1",
+              children:[]
+            },{
+              name:"子类2-2",
+              children:[]
+            },{
+              name:"子类2-3",
+              children:[]
+            },{
+              name:"子类2-4",
+              children:[]
+            }
+          ],
+        },{
+          name:"子类3",
+          children:[
+            {
+              name:"子类3-1",
+              children:[]
+            },{
+              name:"子类3-2",
+              children:[]
+            },{
+              name:"子类3-3",
+              children:[]
+            }
+          ],
+        },{
+          name:"子类4",
+          children:[
+            {
+              name:"子类4-1",
+              children:[]
+            },{
+              name:"子类4-2",
+              children:[]
+            },{
+              name:"子类4-3",
+              children:[]
+            }
+          ],
+        },{
+          name:"子类5",
+          children:[
+            {
+              name:"子类5-1",
+              children:[]
+            },{
+              name:"子类5-2",
+              children:[]
+            },{
+              name:"子类5-3",
+              children:[]
+            }
+          ],
+        },{
+          name:"子类6",
+          children:[
+            {
+              name:"子类6-1",
+              children:[]
+            },{
+              name:"子类6-2",
+              children:[]
+            },{
+              name:"子类6-3",
+              children:[]
+            }
+          ],
+        },{
+          name:"子类6",
+          children:[
+            {
+              name:"子类6-1",
+              children:[]
+            },{
+              name:"子类6-2",
+              children:[]
+            },{
+              name:"子类6-3",
+              children:[]
+            }
+          ],
+        },{
+          name:"子类7",
+          children:[
+            {
+              name:"子类6-1",
+              children:[]
+            },{
+              name:"子类6-2",
+              children:[]
+            },{
+              name:"子类6-3",
+              children:[]
+            }
+          ],
+        },{
+          name:"子类8",
+          children:[
+            {
+              name:"子类6-1",
+              children:[]
+            },{
+              name:"子类6-2",
+              children:[]
+            },{
+              name:"子类6-3",
+              children:[]
+            }
+          ],
+        },{
+          name:"子类9",
+          children:[
+            {
+              name:"子类6-1",
+              children:[]
+            },{
+              name:"子类6-2",
+              children:[]
+            },{
+              name:"子类6-3",
+              children:[]
+            }
+          ],
+        },{
+          name:"子类10",
+          children:[
+            {
+              name:"子类6-1",
+              children:[]
+            },{
+              name:"子类6-2",
+              children:[]
+            },{
+              name:"子类6-3",
+              children:[]
+            }
+          ],
+        },{
+          name:"子类11",
+          children:[
+            {
+              name:"子类6-1",
+              children:[]
+            },{
+              name:"子类6-2",
+              children:[]
+            },{
+              name:"子类6-3",
+              children:[]
+            }
+          ],
+        },{
+          name:"子类11",
+          children:[
+            {
+              name:"子类6-1",
+              children:[]
+            },{
+              name:"子类6-2",
+              children:[]
+            },{
+              name:"子类6-3",
+              children:[]
+            }
+          ],
+        },{
+          name:"子类11",
+          children:[
+            {
+              name:"子类6-1",
+              children:[]
+            },{
+              name:"子类6-2",
+              children:[]
+            },{
+              name:"子类6-3",
+              children:[]
+            }
+          ],
+        },{
+          name:"子类11",
+          children:[
+            {
+              name:"子类6-1",
+              children:[]
+            },{
+              name:"子类6-2",
+              children:[]
+            },{
+              name:"子类6-3",
+              children:[]
+            }
+          ],
+        },{
+          name:"子类11",
+          children:[
+            {
+              name:"子类6-1",
+              children:[]
+            },{
+              name:"子类6-2",
+              children:[]
+            },{
+              name:"子类6-3",
+              children:[]
+            }
+          ],
+        },{
+          name:"子类11",
+          children:[
+            {
+              name:"子类6-1",
+              children:[]
+            },{
+              name:"子类6-2",
+              children:[]
+            },{
+              name:"子类6-3",
+              children:[]
+            }
+          ],
+        },{
+          name:"子类11",
+          children:[
+            {
+              name:"子类6-1",
+              children:[]
+            },{
+              name:"子类6-2",
+              children:[]
+            },{
+              name:"子类6-3",
+              children:[]
+            }
+          ],
+        },{
+          name:"子类11",
+          children:[
+            {
+              name:"子类6-1",
+              children:[]
+            },{
+              name:"子类6-2",
+              children:[]
+            },{
+              name:"子类6-3",
+              children:[]
+            }
+          ],
+        },{
+          name:"子类11",
+          children:[
+            {
+              name:"子类6-1",
+              children:[]
+            },{
+              name:"子类6-2",
+              children:[]
+            },{
+              name:"子类6-3",
+              children:[]
+            }
+          ],
+        },
+      ],
       range_min:0,
       range_max:100,
       area1:1,
@@ -124,8 +464,27 @@ export default {
       ]
     }
   },
+  created:function(){
+    this.choose_node=this.category_list[0];
+  },
   methods:{
-
+    clear_history:function(){
+      console.log("清除历史记录");
+    },
+    handle_folder:function(index){
+      var self=this;
+      // console.log(index);
+      var _children=this.category_list[index];
+      // console.log(_children.children.length);
+      if(!!_children.children&&_children.children.length>0&&index!=0){
+        this.choose_node=_children;
+        this.node_index=index;
+      }else if(index==0){
+        this.choose_node=this.category_list[0];
+        this.node_index=0;
+      }
+      // console.log(this.node_index);
+    }
   },
   components:{
     Tab,
@@ -140,12 +499,34 @@ export default {
     XInput,
     Flexbox,
     FlexboxItem,
-    XButton
+    XButton,
+    Icon
   }
 }
 </script>
 
 <style lang="less">
+.tab-item-content{
+  height: 100%;
+}
+.node-active{
+  background-color:#FB4F5B;
+  color: #fff;
+}
+.tree-box{
+  align-items: flex-start;
+  height: 100%;
+  //overflow-y: hidden;
+  // margin-bottom: 5rem;
+}
+.tree-box-left,.tree-box-right{
+  height: 100%;
+  overflow-y: scroll;
+  // padding-bottom: 7rem;
+}
+.tree-box-left{
+  box-shadow:0 0 5px #aaa;
+}
 .range-content .weui_cell_primary{
   background-color: #ccc;
   width: 4rem;
@@ -159,9 +540,9 @@ export default {
 .category{
   width: 100%;
   height: 100%;
-  .vux-slider > .vux-swiper > .vux-swiper-item{
-    overflow-y: scroll;
-  }
+  padding-bottom: 7rem;
+  box-sizing: border-box;
+  // margin-top:0px;
 }
 .list-search{
   position:absolute;
@@ -174,13 +555,7 @@ export default {
   left: 0;
 }
 
-.category .category-swiper{
-  height:100%;
-  .vux-swiper{
-    height: inherit !important;
-    padding-bottom: 2.7rem;
-  }
-}
+
 .bar.bar-header{
   position: relative;
 }
@@ -189,7 +564,6 @@ export default {
   z-index:10;
   top:0;
   left: 0;
-
 }
 .page-list .vux-tab{
   position: absolute;
