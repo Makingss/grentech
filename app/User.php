43a661c121772b1f3649e2071084578e7f7ed3fa
makingss <wuyanping1029@foxmail.com>
2017-03-14 21:46:59 +0800

    update migration

--- app/User.php
+++ app/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable
 	 * @var array
 	 */
 	protected $fillable = [
-		'name', 'email', 'password', 'is_admin', 'avatar', 'confirmation_token',
+		'name', 'email', 'password', 'is_admin', 'avatar', 'confirmation_token','api_token',
 	];
 
 	/**
