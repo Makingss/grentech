ed621e9b56bb9fc9ecd19710eab9c5aace206d2f
makingss <wuyanping1029@foxmail.com>
2017-03-14 21:41:23 +0800

    update migration

--- database/migrations/2017_03_11_221100_add_api_token_to_users.php
+++ database/migrations/2017_03_11_221100_add_api_token_to_users.php
@@ -26,7 +26,6 @@ class AddApiTokenToUsers extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn(['api_token']);
         });
     }
 }
