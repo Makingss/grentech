cc8809eeda2c02a0db785ef37cadf0d63c6eab1b
王小明 <symenywong@163.com>
2017-03-14 09:46:09 +0800

    aa

--- routes/api.php
+++ routes/api.php
@@ -14,3 +14,28 @@ use Illuminate\Http\Request;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+Route::get('/news', function() {
+//    session(['name'=>'pengbd3']);
+    return [
+        ['id' => 1, 'title' => 'new1'],
+        ['id' => 2, 'title' => 'new2'],
+        ['id' => 3, 'title' => 'new3'],
+        ['id' => 4, 'title' => 'new4'],
+    ];
+});
+Route::get('/newslist', function() {
+    return [
+        ['id' => 1, 'title' => 'new1'],
+        ['id' => 2, 'title' => 'new2'],
+        ['id' => 3, 'title' => 'new3'],
+        ['id' => 4, 'title' => 'new4'],
+    ];
+});
+Route::get('/newsdetail/{id}', function($id,Request $request) {
+    return [
+        'id' => 1,
+        'title' => 'news',
+        'content' => 'content',
+        'created_at' => date('Y-m-d H:i:s')
+    ];
+});
