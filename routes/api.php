1b48507f130107c59efe542462da2f6ab2e04404
makingss <wuyanping1029@foxmail.com>
2017-03-16 20:53:11 +0800

    route api

--- routes/api.php
+++ routes/api.php
@@ -16,13 +16,22 @@ Route::get('/user', function (Request $request) {
 	return $request->user();
 })->middleware('auth:api');
 
+Route::post('/question/follower', 'GoodsController@getGoods')->middleware('auth:api');
+//function (Request $request) {
+//	dd($request->all());
+//	$goods = \App\Admin\Models\Good::where('goods_id',1)->paginate(2)->toJson();
+//	dd($goods);
+//	$comment = \App\Models\Comment::where('id', $request->get('question'))->count();
+//	if ($comment) {
+//		return response()->json(['followed' => true]);
+//	}
+//	return response()->json(['followed' => false]);
+//}
 
-Route::post('/question/follower', function (Request $request) {
-//	return Auth::guard('api')->user();
-	$comment = \App\Models\Comment::where('id', $request->get('question'))->count();
-	if ($comment) {
-		return response()->json(['followed' => true]);
-	}
-	return response()->json(['followed' => false]);
-})->middleware('auth:api');
+Route::get('/brand', 'BrandController@getBrand')->middleware('api');
 
+Route::get('/goods', 'GoodsController@getGoods')->middleware('api');
+
+//Route::post('/api/login',function(Request $request){
+//	dd($request->all());
+//})->middleware('api');
