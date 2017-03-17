e6bddd1c9d21112dd8e50f97082fbe4434c5dd90
makingss <wuyanping1029@foxmail.com>
2017-03-17 12:04:18 +0800

    GoodsController

--- app/Http/Controllers/GoodsController.php
+++ app/Http/Controllers/GoodsController.php
@@ -19,17 +19,17 @@ class GoodsController extends Controller
 	 */
 	public function getGoods(Request $request)
 	{
-//		dd($request->all());
 		$per_page = $request->get('per_page');
 		$relations = $request->get('relations');
+//		$relations = ['mechanics', 'goods_ports', 'assemblies', 'standardfits', 'electrics',
+//			'goods_keywords', 'products', 'brands', 'goods_lv_price', 'member_goods', 'image_attach', 'images'];
 		$parameters = $request->get('parameters');
 		$collection = collect($parameters);
 		$filtered = $collection->only(['brand_id', 'goods_id', 'type_id', 'cat_id', 'bn']);
 		$where = $filtered->all();
-
 		$withRelations = collect($relations);
-		$filteredRelations = $withRelations->only(['Goods_types', 'mechanics', 'goods_ports', 'assemblies', 'standardfits', 'electrics',
-			'goods_keywords', 'products', 'brands', 'goods_lv_price', 'member_goods', 'image_attach', 'images']
+		$filteredRelations = $withRelations->except(['Goods_types', 'mechanics', 'goods_ports', 'assemblies', 'standardfits', 'electrics',
+				'goods_keywords', 'products', 'brands', 'goods_lv_price', 'member_goods', 'image_attach', 'images']
 		);
 		$with = $filteredRelations->all();
 		$goods = Good::with($with)->where($where)->paginate($per_page)->toJson();
