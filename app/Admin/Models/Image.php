8383f1df44529ad23e9fe9cdd1fb8bc509b26c0e
makingss <wuyanping1029@foxmail.com>
2017-03-18 14:33:06 +0800

    image

--- app/Admin/Models/Image.php
+++ app/Admin/Models/Image.php
@@ -9,6 +9,7 @@ class Image extends Model
     protected $table = 'image';
     protected $primaryKey = 'image_id';
     public $incrementing = false;
+    protected $hidden=['storage'];
     protected $fillable = ['image_id', 'storage', 'image_name', 'ident', 'url', 'l_ident', 'l_url', 'm_ident', 'm_url',
         's_ident', 's_url', 'width', 'height', 'watermark', 'build_size', 'last_modified'];
 
