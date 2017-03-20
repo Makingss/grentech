67edf1174c303f2d95fea504ba04d6c62dc362d7
王小明 <symenywong@163.com>
2017-03-20 16:47:50 +0800

    ss

--- resources/views/vue-app/src/api/index.js
+++ resources/views/vue-app/src/api/index.js
@@ -9,10 +9,17 @@ Vue.http.interceptors.push((request, next) => {
 });
 
 const API_ROOT = '';
-
+const TEST_ROOT="http://119.22.23.185"
+console.log(window.location.host);
+var root_host='';
+if(window.location.host=="127.0.0.1"||window.location.host=="localhost:8080"){
+    root_host=TEST_ROOT
+}
 export default {
-    get_api_token:function(){
-        return Vue.http.post(API_ROOT+'/oauth/token',data);
+    get_api_token:function(data){
+        console.log("-----");
+        console.log(root_host);
+        return Vue.http.post(root_host+'/oauth/token',data);
     },
     getGoodsData: function(data) {
         // filtered:[brand_id, goods_id, type_id, cat_id, bn]  
@@ -34,14 +41,14 @@ export default {
           'images'
         ]*/
         // console.log(data);
-        return Vue.http.post(API_ROOT + '/api/goods', data);
+        return Vue.http.post(root_host + '/api/goods', data);
         // return Vue.http.post(API_ROOT+'/api/goods',{relations:"image_attach",parameters:""});
     },
     get_trans_params_table:function(data){
-        return Vue.http.post(API_ROOT+"/table",data);
+        return Vue.http.post(root_host+"/table",data);
     },
     get_cat_list:function(data){
-        return Vue.http.get(API_ROOT+"/goods/cat",data);
+        return Vue.http.get(root_host+"/goods/cat",data);
     }
 
 }
