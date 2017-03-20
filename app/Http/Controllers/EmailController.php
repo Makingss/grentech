ade20d1502e2e803d7cc64b619594aca5f0347a2
makingss <wuyanping1029@foxmail.com>
2017-03-20 17:39:16 +0800

    email

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
+use Laravel\Passport\Client;
+use Laravel\Passport\ClientRepository;
 
 class EmailController extends Controller
 {
@@ -12,14 +15,31 @@ class EmailController extends Controller
 		$user = User::where('confirmation_token', $token)->first();
 		if (is_null($user)) {
 			//flash 提示
-			flash('用户邮箱验证失败！','danger');
+			flash('用户邮箱验证失败！', 'danger');
 			return redirect('/');
 		}
+		$this->create($user->id, $user->email, $user->email, '');
 		$user->is_active = 1;
 		$user->confirmation_token = str_random(40);
 		$user->save();
-		flash('欢迎回来！','success');
+		flash('欢迎回来！', 'success');
 		return redirect('/home');
 
+	}
+
+	public function create($userId, $name, $secret, $redirect, $personalAccess = false, $password = false)
+	{
+		$client = (new Client())->forceFill([
+			'user_id' => $userId,
+			'name' => $name,
+			'secret' => $secret,
+			'redirect' => $redirect,
+			'personal_access_client' => $personalAccess,
+			'password_client' => $password,
+			'revoked' => false,
+		]);
+
+		$client->save();
+		return $client;
 	}
 }
