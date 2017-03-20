43c8698af2c1ef9a10531d22bc338eb693236b17
王小明 <symenywong@163.com>
2017-03-20 18:09:26 +0800

    ss

--- resources/views/vue-app/src/components/vchart.vue
+++ resources/views/vue-app/src/components/vchart.vue
@@ -1,44 +1,58 @@
 <template>
     <div class="chart">
-        <canvas id="c1" width="200" height="300"></canvas>
+      <div id="c1"></div>
     </div>
 
 </template>
 <script>
+    import G2 from 'g2';
     // import GM from 'g2-mobile'
-    var GM = require('g2-mobile');
     export default {
         name:'vchart',
         components:{
-            GM
+            G2
         },
         data:function(){
             return {
-                vchart_data:[
-                    {genre: 'Sports', sold: 275},
-                    {genre: 'Strategy', sold: 115},
-                    {genre: 'Action', sold: 120},
-                    {genre: 'Shooter', sold: 350},
-                    {genre: 'Other', sold: 150}
-                ]
+                serverData: []
             }
+            
+        },
+        methods:{
+           draw_chart:function(){
+               var data = [
+                {genre: 'Sports', sold: 275},
+                {genre: 'Strategy', sold: 115},
+                {genre: 'Action', sold: 120},
+                {genre: 'Shooter', sold: 350},
+                {genre: 'Other', sold: 150},
+                ]; // G2 对数据源格式的要求，仅仅是 JSON 数组，数组的每个元素是一个标准 JSON 对象。
+                // Step 1: 创建 Chart 对象
+                var chart = new G2.Chart({
+                    id: 'c1', // 指定图表容器 ID
+                    width : 600, // 指定图表宽度
+                    height : 300 // 指定图表高度
+                });
+                // Step 2: 载入数据源
+                chart.source(data, {
+                genre: {
+                    alias: '游戏种类' // 列定义，定义该属性显示的别名
+                },
+                sold: {
+                    alias: '销售量'
+                }
+                });
+                // Step 3：创建图形语法，绘制柱状图，由 genre 和 sold 两个属性决定图形位置，genre 映射至 x 轴，sold 映射至 y 轴
+                chart.interval().position('genre*sold').color('genre')
+                // Step 4: 渲染图表
+                chart.render();
+           }
+        },
+        mounted:function(){
+           
         },
         created:function(){
-            // console.log(GM);
-            // //  var Util = GM.Util;
-            // var chart = new GM.Chart({
-            //     id: 'c1',
-            //     width: 600,
-            //     height: 300
-            // });
-            // var data = [
-            //     {"tem": 10, "city": "tokyo"},
-            //     {"tem": 4, "city": "newYork"},
-            //     {"tem": 3, "city": "berlin"}
-            // ];
-            // chart.source(data);
-            // chart.interval().position('city*tem').color('city');
-            // chart.render();
+           this. draw_chart();
         }
 
     }
