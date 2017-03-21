e9068472b01e6961bb0768de3ecfc9ee6f5b2894
王小明 <symenywong@163.com>
2017-03-21 21:31:12 +0800

    s

--- resources/views/vue-app/src/components/goods.vue
+++ resources/views/vue-app/src/components/goods.vue
@@ -9,16 +9,17 @@
       <flexbox :gutter="0" wrap="nowrap" class="bg-white">
         <flexbox-item class="padding-tb-6 padding-l-10 border-box" :span="9">
           <p class="line-ellispse-2">{{goods_data_list.name}}</p>
+          <p class="color-danger">¥{{goods_data_list.bn}}</p>
           <p class="color-danger">¥{{goods_data_list.price}}</p>
           <p class="color-gray">市场价:
-            <s>{{goods_data_list.mktprice}}</s>
+            <s>{{goods_data_list.mktprice||'暂无'}}</s>
           </p>
         </flexbox-item>
         <flexbox-item :span="3" class="link-img padding-rl-6 border-box">
           <img src="/static/slice/code.jpg" alt="">
         </flexbox-item>
       </flexbox>
-      <div class="padding-10 margin-tb-10 bg-white">
+      <div class="padding-10 margin-tb-10 bg-white" v-if="false">
         <flexbox :gutter="0" wrap="nowrap">
           <flexbox-item :span="11">
             <p>颜色: <span>红色</span><span>藏蓝</span><span>黑色</span><span>粉红</span></p>
@@ -43,27 +44,27 @@
             <div v-html="goods_data_list.content"></div>
           </swiper-item>
           <swiper-item >
-               <div @click="collapse(1)" class="collapse_title color-danger padding-rl-10 padding-tb-6">端口</div>
+               <div @click="collapse(1)" class="collapse_title color-danger bg-sliver padding-rl-10 padding-tb-6">端口</div>
               <group class="margin-0" v-for="(item,index) in goods_data_list.goods_ports" v-show="collapse1">
                 <cell v-for="(item1,index1) in item" class="font-normal" :title="parms_table.goods_ports[index1]||index1" :value="item1" v-if="item1&&item1!='0'&&index1!='created_at'&&index1!='updated_at'&&index1!='goods_id'&&index1!='id'">
                 </cell>
               </group>
-               <div @click="collapse(2)" class="collapse_title color-danger padding-rl-10 padding-tb-6">电信号</div>
+               <div @click="collapse(2)" class="collapse_title color-danger bg-sliver padding-rl-10 padding-tb-6">电信号</div>
                <group class="margin-0" v-for="(item,index) in goods_data_list.electrics" v-show="collapse2">
                 <cell v-for="(item1,index1) in item" class="font-normal" :title="parms_table.electrics[index1]||index1" :value="item1" v-if="item1&&item1!='0'&&index1!='created_at'&&index1!='updated_at'&&index1!='goods_id'&&index1!='id'">
                 </cell>
               </group>
-               <div @click="collapse(3)" class="collapse_title color-danger padding-rl-10 padding-tb-6">组合</div>
+               <div @click="collapse(3)" class="collapse_title color-danger bg-sliver padding-rl-10 padding-tb-6">组合</div>
               <group class="margin-0" v-for="(item,index) in goods_data_list.assemblies" v-show="collapse3">
                 <cell v-for="(item1,index1) in item" class="font-normal" :title="parms_table.assemblies[index1]||index1" :value="item1" v-if="item1&&item1!='0'&&index1!='created_at'&&index1!='updated_at'&&index1!='goods_id'&&index1!='id'">
                 </cell>
               </group>
-               <div @click="collapse(4)" class="collapse_title color-danger padding-rl-10 padding-tb-6">标准</div>
+               <div @click="collapse(4)" class="collapse_title color-danger bg-sliver padding-rl-10 padding-tb-6">标准</div>
               <group class="margin-0" v-for="(item,index) in goods_data_list.standardfits" v-show="collapse4">
                 <cell v-for="(item1,index1) in item" class="font-normal" :title="parms_table.standardfits[index1]||index1" :value="item1" v-if="item1&&item1!='0'&&index1!='created_at'&&index1!='updated_at'&&index1!='goods_id'&&index1!='id'">
                 </cell>
               </group>
-                <div @click="collapse(5)" class="collapse_title color-danger padding-rl-10 padding-tb-6">机械性能</div>
+                <div @click="collapse(5)" class="collapse_title color-danger bg-sliver padding-rl-10 padding-tb-6">机械性能</div>
                <group class="margin-0" v-show="collapse5">
                 <cell v-for="(item,index) in goods_data_list.mechanics" class="font-normal" :title="parms_table.mechanics[index]||index" :value="item" v-if="item&&item!='0'&&index!='created_at'&&index!='updated_at'&&index!='goods_id'&&index!='id'">
                 </cell>
@@ -134,62 +135,29 @@ import {mapState,mapActions} from 'vuex'
     }),
     methods: {
       collapse:function(index){
-        console.log(index);
         this["collapse"+index]=!this["collapse"+index];
       },
       ...mapActions(['GETGOODSLIST']),
       init_goods_page: function (init_data) {
-        // console.log()
-        // if (!!init_data.goods_list.data && init_data.goods_list.data.length) {
-        //   var page_goods_data = init_data.goods_list;
-        //   this.current_page = page_goods_data.current_page;
-        //   this.goods_data_list = page_goods_data.data[this.item_index];
-        //   this.from = page_goods_data.from;
-        //   this.last_page = page_goods_data.last_page;
-        //   this.per_page = page_goods_data.per_page;
-        //   this.to = page_goods_data.to;
-        //   this.total = page_goods_data.total;
-        // }else{
-        //   var self=this;
-        //   // this.GETGOODSLIST({relations: ["image_attach", "images"], parameters:{goods_id:39}});
-        //   api.getGoodsData({relations: ["image_attach", "images","mechanics","goods_ports","assemblies","standardfits","electrics"], parameters:{goods_id:39}}).then((res)=>{
-        //     console.log(res);
-        //     self.goods_data_list=res.data.data[0];
-        //   });
-          
-        //   // console.log("********");
-        //   // console.log(this.$store.state.goods);
-        //   // this.temp_data=this.$store.state.goods;
-        //   // this.goods_data_list=this.$store.state.goods.goods_list.data[0];
-        //   // console.log(this.goods_data_list);
-        //   // // console.log(this.$store.state.goods["goods_list"]);
-        //   // // console.log(this.$store.state.goods["goods_list"].data);
-        //   // console.log(this.temp_data);
-        // }
-        
          var self=this;
           // this.GETGOODSLIST({relations: ["image_attach", "images"], parameters:{goods_id:39}});
           api.getGoodsData({relations: ["image_attach", "images","mechanics","goods_ports","assemblies","standardfits","electrics"], parameters:{goods_id:39}}).then((res)=>{
-            console.log(res);
             self.goods_data_list=res.data.data[0];
           });
-       console.log("laqu ");
       },
       get_parms_data:function(){
         var self=this;
         api.get_trans_params_table({relations: ['mechanics','goods_ports','assemblies','standardfits','electrics',]}).then((res)=>{
-          console.log(res);
           self.parms_table=res.data;
         });
       }
     },
     created: function () {
-      console.log("created");
       var query = this.$route.query;
       this.goods_id = query.goods_id;
       this.item_index = query.item_index;
       this.init_goods_page(this.$store.state.goods);
-       this.get_parms_data();
+      this.get_parms_data();
     },
     components: {
       Swiper,
