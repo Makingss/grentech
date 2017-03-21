311316c08ba1d6cd188e400abb60920d846108f2
王小明 <symenywong@163.com>
2017-03-21 09:28:15 +0800

    kk

--- resources/views/vue-app/src/components/vchart.vue
+++ resources/views/vue-app/src/components/vchart.vue
@@ -1,6 +1,8 @@
 <template>
-    <div class="chart">
+    <div class="chart content">
       <canvas id="chart"></canvas>
+      <canvas id="circlechart"></canvas>
+      <canvas id="rangechart"></canvas>
     </div>
 </template>
 <script>
@@ -21,7 +23,6 @@
            draw_chart:function(){
                GM.Global.pixelRatio = 2;
                var Util = GM.Util;
-                
                 var data = [
                     {"time": '周一',"tem": 10,"city": "beijing"},
                     {"time": '周二',"tem": 22,"city": "beijing"},
@@ -77,10 +78,100 @@
                  chart.source(data, defs);
                 chart.line().position('time*tem').color('city').shape('smooth');
                 chart.render();
+           },
+           draw_circle_chart:function(){
+               GM.Global.pixelRatio = 2;
+                var  data = [
+                    {a: '1', b: 0.3, c: '1'},
+                    {a: '1', b: 0.3, c: '2'},
+                    {a: '1', b: 0.4, c: '3'}
+                ];
+                var chart = new GM.Chart({
+                    id: 'circlechart'
+                });
+                chart.source(data);
+                chart.coord('polar', {
+                    transposed: true,
+                    inner: 0
+                });
+                chart.axis(false);
+                chart.intervalStack().position('a*b').color('c');
+                chart.render();
+           },
+           draw_range_chart:function(){
+                 var Util = GM.Util;
+                // 双精度
+                GM.Global.pixelRatio = 2;   
+                var data = [
+                    {"time": '2016-08-08 00:00:00',"tem": 10,"city": "beijing"},
+                    {"time": '2016-08-08 00:10:00',"tem": 22,"city": "beijing"},
+                    {"time": '2016-08-08 00:30:00',"tem": 16,"city": "beijing"},
+                    {"time": '2016-08-09 00:35:00',"tem": 26,"city": "beijing"},
+                    {"time": '2016-08-09 01:00:00',"tem": 12,"city": "beijing"},
+                    {"time": '2016-08-09 01:20:00',"tem": 26,"city": "beijing"},
+                    {"time": '2016-08-10 01:40:00',"tem": 18,"city": "beijing"},
+                    {"time": '2016-08-10 02:00:00',"tem": 26,"city": "beijing"},
+                    {"time": '2016-08-10 02:20:00',"tem": 12,"city": "beijing"},
+                    {"time": '2016-08-08 00:00:00',"tem": 28,"city": "newYork"},
+                    {"time": '2016-08-08 00:10:00',"tem": 16,"city": "newYork"},
+                    {"time": '2016-08-08 00:30:00',"tem": 26,"city": "newYork"},
+                    {"time": '2016-08-09 00:35:00',"tem": 12,"city": "newYork"},
+                    {"time": '2016-08-09 01:00:00',"tem": 26,"city": "newYork"},
+                    {"time": '2016-08-09 01:20:00',"tem": 20,"city": "newYork"},
+                    {"time": '2016-08-10 01:40:00',"tem": 29,"city": "newYork"},
+                    {"time": '2016-08-10 02:00:00',"tem": 16,"city": "newYork"},
+                    {"time": '2016-08-10 02:20:00',"tem": 22,"city": "newYork"}
+                ];
+                var chart = new GM.Chart({
+                    id: 'rangechart'
+                });
+                chart.source(data, {
+                    time: {
+                    type: 'timeCat',
+                    mask: 'yyyy-mm-dd',
+                    tickCount: 3,
+                    range:[0,1]
+                    },
+                    tem: {
+                    tickCount: 5,
+                    min: 0
+                    }
+                });
+                //配置刻度文字大小，供PC端显示用(移动端可以使用默认值20px)
+                chart.axis('tem', {
+                    label: {
+                    fontSize: 14 
+                    }
+                });
+                //配置time刻度文字样式
+                var label = {
+                    fill: '#979797',
+                    font: '14px san-serif',
+                    offset: 6
+                };
+                chart.axis('time', {
+                    label: function(text, index, total) {
+                    var cfg = Util.mix({}, label);
+                    // 第一个点左对齐，最后一个点右对齐，其余居中，只有一个点时左对齐
+                    if (index === 0) {
+                        cfg.textAlign = 'start';
+                    }
+                    if (index > 0 && index === total - 1) {
+                        cfg.textAlign = 'end';
+                    }
+                    return cfg;
+                    }
+                });
+                chart.area().position('time*tem').color('city').shape('smooth').style({
+                    opacity: 0.6
+                });
+                chart.render();
            }
         },
         mounted:function(){
            this. draw_chart();
+           this.draw_circle_chart();
+           this.draw_range_chart();
         },
         created:function(){
            
@@ -88,9 +179,12 @@
     }
 </script>
 <style lang="css" scoped>
-    #chart{
+    #chart,#circlechart,#rangechart{
         width:105%;
         height:260px;
-        margin-top:1.5rem;
+        margin-top:.5rem;
     }
+   #rangechart{
+       margin-bottom:2rem;
+   }
 </style>
