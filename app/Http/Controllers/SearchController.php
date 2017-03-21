b2873c7e0dab8051be794331f325af5e41dbb4ad
makingss <wuyanping1029@foxmail.com>
2017-03-22 01:52:32 +0800

    Search

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -11,17 +11,17 @@ class SearchController extends Controller
 	public function index(Request $request)
 	{
 		$per_page = $request->get('per_page');
-		$relations = $request->get('relations') ? $request->get('relations') : 'image_attach,goods_keywords';
+		$relations = $request->get('relations') ? $request->get('relations') : 'image_attach';
 		$query = $request->get('search');
 //		$collection = collect($parameters);
 //		$filtered = $collection->only(['brand_id', 'goods_id', 'type_id', 'cat_id', 'bn']);
 //		$where = $filtered->all();
 		$withRelations = collect($relations);
-		$filteredRelations = $withRelations->except(['Goods_types', 'mechanics', 'goods_ports', 'assemblies', 'standardfits', 'electrics',
+		$filteredRelations = $withRelations->except(['goods_types', 'mechanics', 'goods_ports', 'assemblies', 'standardfits', 'electrics',
 				'goods_keywords', 'products', 'brands', 'goods_lv_price', 'member_goods', 'image_attach', 'images', 'goods_cats']
 		);
 		$with = $filteredRelations->all();
-		$goods = Good::search($query)->with(['image_attach','goods_keywords'])->paginate($per_page)->toArray();
+		$goods = Good::search($query)->with($with)->paginate($per_page)->toArray();
 		foreach ($goods['data'] as $dataK => $data) {
 			foreach ($data['image_attach'] as $itemK => $item) {
 				$image_attach = Image_attach::with('images')->where('image_id', $item['image_id'])->get()->toArray();
@@ -31,7 +31,6 @@ class SearchController extends Controller
 
 			}
 		}
-		dd($goods);
 		return json_encode($goods);
 
 	}
