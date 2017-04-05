846a3433725322a9b0065fdaa96e02a420ae8a95
王小明 <symenywong@163.com>
2017-04-05 13:54:17 +0800

    增加性能参数显示问题

---
+++ resources/assets/js/components/Alert.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <group>
+            <x-switch title="Show Me" v-model="show"></x-switch>
+        </group>
+        <div>
+            <!--<alert v-model="show" title="Congratulations" >Your Message is sent successfully~</alert>-->
+        </div>
+
+    </div>
+    </div>
+</template>
+
+
+<script>
+    import { Alert, Group, XSwitch, Cell } from 'vux'
+
+    export default {
+        directives: {
+//            TransferDom
+        },
+        components: {
+            Alert,
+            Group,
+            XSwitch,
+            Cell
+        },
+        data () {
+            return {
+                show: false,
+            }
+        },
+        methods: {
+            onShow () {
+                console.log('on show')
+            },
+            onHide () {
+                console.log('on hide')
+            }
+        }
+    }
+</script>
