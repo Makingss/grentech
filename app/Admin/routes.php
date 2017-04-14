8fe83a6f4ea723373627e4ec6e76d0a0a226aa00
makingss <wuyanping1029@foxmail.com>
2017-04-14 14:17:26 +0800

    routes.php

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -7,19 +7,11 @@ Route::group([
     'namespace' => Admin::controllerNamespace(),
     'middleware' => ['web', 'admin'],
 ], function (Router $router) {
-<<<<<<< HEAD
-	$router->get('/', 'IndexController@index');
-	$router->get('/goods/getgoods', 'Goods\GoodsController@getgoods');
-	$router->get('/goods/getindex', 'Goods\GoodsController@getindex');
-	$router->post('/fileupload', 'Ectools\ToolsbaseController@fileUpload');
-	$router->post('/fileupload/remove', 'Ectools\ToolsbaseController@remove');
-=======
     $router->get('/', 'IndexController@index');
     $router->get('/goods/getgoods', 'Goods\GoodsController@getgoods');
     $router->get('/goods/getindex', 'Goods\GoodsController@getindex');
     $router->post('/fileupload', 'Ectools\ToolsbaseController@fileUpload');
     $router->post('/fileupload/remove', 'Ectools\ToolsbaseController@remove');
->>>>>>> 8f150f93803842a9fab19781fb40b5430c98e0e9
 
 //    $router->post('/goods/update_z','GoodsController@update_z');
     $router->resource('/painter', 'PainterController');
@@ -28,20 +20,7 @@ Route::group([
     $router->resource('goodscat', 'Goods\GoodsCatController');
     $router->resource('/articles', 'ArticleController');
 //	$router->resource('/wangeditor', 'WangEditorController');
-<<<<<<< HEAD
-	$router->resource('/product', 'Products\ProductController');
-	$router->resource('/brand', 'Goods\BrandController');
-	$router->resource('/spec', 'Products\SpecificationController');
-	$router->get('/product/{product}/create', 'Products\ProductController@create');
-	$router->get('/specvalue/values/{spec_id}', 'Products\SpecvaluesController@spec_value');
-	$router->post('/specvalue/specvalueeditor', 'Products\SpecvaluesController@specvalue_editor');
-	$router->get('/electric/getindex/{id}', 'Goods\ElectricController@getIndex');
-	$router->post('/electric/setajax', 'Goods\ElectricController@setAjax');
-	Route::group(['namespace'=>'Orders'],function(){
-		Route::resource('/orders','OrderController');
-	});
 //	$router->resource('/orders',);
-=======
     $router->resource('/product', 'Products\ProductController');
     $router->resource('/brand', 'Goods\BrandController');
     $router->resource('/spec', 'Products\SpecificationController');
@@ -51,7 +30,6 @@ Route::group([
     $router->get('/electric/getindex/{id}', 'Goods\ElectricController@getIndex');
     $router->post('/electric/setajax', 'Goods\ElectricController@setAjax');
     //$router->resource('auth/users', UserController::class);
->>>>>>> 8f150f93803842a9fab19781fb40b5430c98e0e9
 //	$router->get('/products/{key?}', 'DatatablesController@index');
 //	$router->get('/products/data/{goods_id}', 'DatatablesController@anyData');
 //	$router->post('/products/editor', 'DatatablesController@editor');
