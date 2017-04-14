8f150f93803842a9fab19781fb40b5430c98e0e9
pengbd3 <pengbd3@163.com>
2017-04-14 11:44:32 +0800

    后台管理登录

---
+++ app/Admin/Controllers/UserController.php
@@ -0,0 +1,171 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use Encore\Admin\Auth\Database\Administrator;
+use Encore\Admin\Auth\Database\Permission;
+use Encore\Admin\Auth\Database\Role;
+use Encore\Admin\Controllers\ModelForm;
+use Encore\Admin\Facades\Admin;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Layout\Content;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Input;
+use Laravel\Passport\Client;
+
+class UserController extends Controller
+{
+    use ModelForm;
+
+    /**
+     * Index interface.
+     *
+     * @return Content
+     */
+    public function index()
+    {
+        return Admin::content(function (Content $content) {
+            $content->header(trans('admin::lang.administrator'));
+            $content->description(trans('admin::lang.list'));
+            $content->body($this->grid()->render());
+        });
+    }
+
+    /**
+     * Edit interface.
+     *
+     * @param $id
+     *
+     * @return Content
+     */
+    public function edit($id)
+    {
+        return Admin::content(function (Content $content) use ($id) {
+            $content->header(trans('admin::lang.administrator'));
+            $content->description(trans('admin::lang.edit'));
+            $content->body($this->form()->edit($id));
+        });
+    }
+
+    /**
+     * Create interface.
+     *
+     * @return Content
+     */
+    public function create()
+    {
+        return Admin::content(function (Content $content) {
+            $content->header(trans('admin::lang.administrator'));
+            $content->description(trans('admin::lang.create'));
+            $content->body($this->form());
+        });
+    }
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        return Administrator::grid(function (Grid $grid) {
+            $grid->id('ID')->sortable();
+            $grid->username(trans('admin::lang.username'));
+            $grid->name(trans('admin::lang.name'));
+            $grid->roles(trans('admin::lang.roles'))->pluck('name')->label();
+            $grid->created_at(trans('admin::lang.created_at'));
+            $grid->updated_at(trans('admin::lang.updated_at'));
+
+            $grid->actions(function (Grid\Displayers\Actions $actions) {
+                if ($actions->getKey() == 1) {
+                    $actions->disableDelete();
+                }
+            });
+
+            $grid->tools(function (Grid\Tools $tools) {
+                $tools->batch(function (Grid\Tools\BatchActions $actions) {
+                    $actions->disableDelete();
+                });
+            });
+
+            $grid->disableExport();
+        });
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    public function form()
+    {
+        return Administrator::form(function (Form $form) {
+            $form->display('id', 'ID');
+
+            $form->text('username', trans('admin::lang.username'))->rules('required');
+            $form->text('name', trans('admin::lang.name'))->rules('required');
+            $form->image('avatar', trans('admin::lang.avatar'));
+            $form->password('password', trans('admin::lang.password'))->rules('required|confirmed');
+            $form->password('password_confirmation', trans('admin::lang.password_confirmation'))->rules('required')
+                ->default(function ($form) {
+                    return $form->model()->password;
+                });
+
+            $form->ignore(['password_confirmation']);
+
+            $form->multipleSelect('roles', trans('admin::lang.roles'))->options(Role::all()->pluck('name', 'id'));
+            $form->multipleSelect('permissions', trans('admin::lang.permissions'))->options(Permission::all()->pluck('name', 'id'));
+
+            $form->display('created_at', trans('admin::lang.created_at'));
+            $form->display('updated_at', trans('admin::lang.updated_at'));
+//            dd($form->text('username'));
+//            dd($form);
+            $form->saving(function (Form $form) {
+                if ($form->password && $form->model()->password != $form->password) {
+                    $form->password = bcrypt($form->password);
+                }
+            });
+        });
+    }
+
+    public function store(Request $request)
+    {
+        return $this->form()->store($request);
+    }
+
+    public function update(Request $request, $id)
+    {
+//        dd($request->all());
+//        $store=$this->form()->update($request);
+//        dd($request->all());
+        return $this->form()->update($id);
+    }
+
+    /**
+     * 重写Oauth的客户端创建
+     * @param integer $userId 用户ID
+     * @param string $name 用户名
+     * @param string $secret secret_id
+     * @param string $redirect 回调地址
+     * @param bool $personalAccess 是否私钥
+     * @param bool $password 是否公钥
+     * @return mixed
+     */
+    public function oauthClientCreate($userId, $name, $secret, $redirect, $personalAccess = false, $password = true)
+    {
+        $client = (new Client())->forceFill([
+            'user_id' => $userId,
+            'name' => $name,
+            'secret' => md5($secret . $userId),
+            'redirect' => $redirect,
+            'personal_access_client' => $personalAccess,
+            'password_client' => $password,
+            'revoked' => false,
+        ]);
+
+        $client->save();
+        return $client;
+    }
+}
--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -3,32 +3,32 @@
 use Illuminate\Routing\Router;
 
 Route::group([
-	'prefix' => config('admin.prefix'),
-	'namespace' => Admin::controllerNamespace(),
-	'middleware' => ['web', 'admin'],
+    'prefix' => config('admin.prefix'),
+    'namespace' => Admin::controllerNamespace(),
+    'middleware' => ['web', 'admin'],
 ], function (Router $router) {
-	$router->get('/', 'IndexController@index');
-	$router->get('/goods/getgoods', 'Goods\GoodsController@getgoods');
-	$router->get('/goods/getindex', 'Goods\GoodsController@getindex');
-	$router->post('/fileupload', 'Ectools\ToolsbaseController@fileUpload');
-	$router->post('/fileupload/remove', 'Ectools\ToolsbaseController@remove');
-	
-//    $router->post('/goods/update_z','GoodsController@update_z');
-	$router->resource('/painter', 'PainterController');
-	$router->resource('/goods', 'Goods\GoodsController');
-	$router->resource('/goodstype', 'Goods\GoodsTypeController');
-	$router->resource('goodscat', 'Goods\GoodsCatController');
-	$router->resource('/articles', 'ArticleController');
-//	$router->resource('/wangeditor', 'WangEditorController');
-	$router->resource('/product', 'Products\ProductController');
-	$router->resource('/brand', 'Goods\BrandController');
-	$router->resource('/spec', 'Products\SpecificationController');
-	$router->get('/product/{product}/create', 'Products\ProductController@create');
-	$router->get('/specvalue/values/{spec_id}', 'Products\SpecvaluesController@spec_value');
-	$router->post('/specvalue/specvalueeditor', 'Products\SpecvaluesController@specvalue_editor');
-	$router->get('/electric/getindex/{id}', 'Goods\ElectricController@getIndex');
-	$router->post('/electric/setajax', 'Goods\ElectricController@setAjax');
+    $router->get('/', 'IndexController@index');
+    $router->get('/goods/getgoods', 'Goods\GoodsController@getgoods');
+    $router->get('/goods/getindex', 'Goods\GoodsController@getindex');
+    $router->post('/fileupload', 'Ectools\ToolsbaseController@fileUpload');
+    $router->post('/fileupload/remove', 'Ectools\ToolsbaseController@remove');
 
+//    $router->post('/goods/update_z','GoodsController@update_z');
+    $router->resource('/painter', 'PainterController');
+    $router->resource('/goods', 'Goods\GoodsController');
+    $router->resource('/goodstype', 'Goods\GoodsTypeController');
+    $router->resource('goodscat', 'Goods\GoodsCatController');
+    $router->resource('/articles', 'ArticleController');
+//	$router->resource('/wangeditor', 'WangEditorController');
+    $router->resource('/product', 'Products\ProductController');
+    $router->resource('/brand', 'Goods\BrandController');
+    $router->resource('/spec', 'Products\SpecificationController');
+    $router->get('/product/{product}/create', 'Products\ProductController@create');
+    $router->get('/specvalue/values/{spec_id}', 'Products\SpecvaluesController@spec_value');
+    $router->post('/specvalue/specvalueeditor', 'Products\SpecvaluesController@specvalue_editor');
+    $router->get('/electric/getindex/{id}', 'Goods\ElectricController@getIndex');
+    $router->post('/electric/setajax', 'Goods\ElectricController@setAjax');
+    //$router->resource('auth/users', UserController::class);
 //	$router->get('/products/{key?}', 'DatatablesController@index');
 //	$router->get('/products/data/{goods_id}', 'DatatablesController@anyData');
 //	$router->post('/products/editor', 'DatatablesController@editor');
