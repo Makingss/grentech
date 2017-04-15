25ba5905f74526273fda5988d0ec98741a26b739
pengbd3 <pengbd3@163.com>
2017-04-15 20:48:55 +0800

    添加登录api路由

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -28,6 +28,9 @@ Route::group([
     $router->post('/specvalue/specvalueeditor', 'Products\SpecvaluesController@specvalue_editor');
     $router->get('/electric/getindex/{id}', 'Goods\ElectricController@getIndex');
     $router->post('/electric/setajax', 'Goods\ElectricController@setAjax');
+
+    $router->resource('/auth/users',UserController::class);
+
     Route::group(['namespace'=>'Orders'],function(){
         Route::resource('/orders','OrderController');
     });
