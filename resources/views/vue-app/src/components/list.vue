60f3f24709d47852313b24ec1819d900716375b2
王小明 <symenywong@163.com>
2017-04-18 18:09:00 +0800

    修正字体毛边

--- resources/views/vue-app/src/components/list.vue
+++ resources/views/vue-app/src/components/list.vue
@@ -1,85 +1,85 @@
 <template lang="html">
   <div class="template-content">
-  <div class="content-box">
-      <div class="search-icon text-center" @click="toggleType">
-        <span class="iconfont font-3x color-success block" style="margin-top:8px;">&#xe62a;</span>
-      </div>
-      <search v-model="search_input" position="static" top="0" @on-submit="submit_search"  class="list-search border-box"></search>
-      <div class="content list container" @scroll="handle_scroll($event)">
-            <flexbox wrap="wrap" :gutter="0" class="scroll-content" v-if="type=='large'">
-              <flexbox-item :data-currentpage="current_page" :data-lastpage="last_page"  :data-total="total"  :data-perpage="per_page" :data-i="index%2"
-              v-for="(item,index) in goods_data" :span="1/2" class="link-img cell-list-2 padding-tb-6 border-box" :class="{'padding-r-2':index%2==0,'padding-l-2':index%2==1}" >
-                <router-link :to="{name:'goods',query:{goods_id:item.goods_id,item_index:index}}" class="block">
-                  <div>
-                    <img :src="item.images?item.images.url:'/static/grentech/default.jpg'" alt="">
-                  </div>
-                  <div class="padding-rl-10">
-                    <div class="item-title line-ellispse-2">
-                      {{item.name}}
+    <div class="content-box">
+        <div class="search-icon text-center" @click="toggleType">
+          <span class="iconfont font-3x color-success block" style="margin-top:8px;">&#xe62a;</span>
+        </div>
+        <search v-model="search_input" position="static" top="0" @on-submit="submit_search"  class="list-search border-box"></search>
+        <div class="content list container" @scroll="handle_scroll($event)">
+              <flexbox wrap="wrap" :gutter="0" class="scroll-content" v-if="type=='large'">
+                <flexbox-item :data-currentpage="current_page" :data-lastpage="last_page"  :data-total="total"  :data-perpage="per_page" :data-i="index%2"
+                v-for="(item,index) in goods_data" :span="1/2" class="link-img cell-list-2 padding-tb-6 border-box" :class="{'padding-r-2':index%2==0,'padding-l-2':index%2==1}" >
+                  <router-link :to="{name:'goods',query:{goods_id:item.goods_id,item_index:index}}" class="block">
+                    <div>
+                      <img :src="item.images?item.images.url:'/static/grentech/default.jpg'" alt="">
                     </div>
-                    <div class="item-title line-ellispse-2 font-bold" v-if="!!item.electrics">
-                      频段: 
-                      <span v-for="(_item,_index) in item.electrics" v-if="!!_item.workingband">
-                      {{_item.workingband}}
-                      <i v-if="(_index!=item.electrics.length-1)&&!!item.electrics[_index+1].workingband">/</i>
-                      </span> 
-                      <span v-if="item.electrics.length">M</span>
+                    <div class="padding-rl-10">
+                      <div class="item-title line-ellispse-2">
+                        {{item.name}}
+                      </div>
+                      <div class="item-title line-ellispse-2 font-bold" v-if="!!item.electrics">
+                        频段: 
+                        <span v-for="(_item,_index) in item.electrics" v-if="!!_item.workingband">
+                        {{_item.workingband}}
+                        <i v-if="(_index!=item.electrics.length-1)&&!!item.electrics[_index+1].workingband">/</i>
+                        </span> 
+                        <span v-if="item.electrics.length">M</span>
+                      </div>
+                      <div class="item-title line-ellispse-2 font-bold">
+                        增益: <span v-for="(_item,_index) in item.electrics" v-if="!!_item.beamgain">{{_item.beamgain}}<i v-if="(_index!=item.electrics.length-1)&&!!item.electrics[_index+1].beamgain">/</i></span>
+                        <span v-if="item.electrics.length">dBi</span>
+                      </div>
+                      <div class="item-title line-ellispse-2 font-bold">
+                        电下倾: <span v-for="(_item,_index) in item.electrics" v-if="!!_item.dipangle">{{_item.dipangle}}<i v-if="(_index!=item.electrics.length-1)&&!!item.electrics[_index+1].dipangle">/</i></span>
+                        <span v-if="item.electrics.length">°</span>
+                      </div>
+                      <div class="item-title line-ellispse-2 color-gray">
+                        SAP: {{item.bn}}
+                      </div>
+                      <div class="item-subtitle padding-t-4">
+                        <b class="color-danger">￥{{item.price}}</b>
+                        <s class="padding-r-10 font-slim color-gray" v-if="item.mktprice">¥</s>
+                        <s class="color-gray font-slim">{{item.mktprice||"暂无"}}</s>
+                      </div>
                     </div>
-                    <div class="item-title line-ellispse-2 font-bold">
-                      增益: <span v-for="(_item,_index) in item.electrics" v-if="!!_item.beamgain">{{_item.beamgain}}<i v-if="(_index!=item.electrics.length-1)&&!!item.electrics[_index+1].beamgain">/</i></span>
-                      <span v-if="item.electrics.length">dBi</span>
-                    </div>
-                    <div class="item-title line-ellispse-2 font-bold">
-                      电下倾: <span v-for="(_item,_index) in item.electrics" v-if="!!_item.dipangle">{{_item.dipangle}}<i v-if="(_index!=item.electrics.length-1)&&!!item.electrics[_index+1].dipangle">/</i></span>
-                      <span v-if="item.electrics.length">°</span>
-                    </div>
-                    <div class="item-title line-ellispse-2 color-gray">
-                      SAP: {{item.bn}}
-                    </div>
-                    <div class="item-subtitle padding-t-4">
-                      <b class="color-danger">￥{{item.price}}</b>
-                      <s class="padding-r-10 font-slim color-gray" v-if="item.mktprice">¥</s>
-                      <s class="color-gray font-slim">{{item.mktprice||"暂无"}}</s>
-                    </div>
-                  </div>
+                  </router-link>
+                </flexbox-item>
+              </flexbox>
+              <card-list 
+                v-for="(item,index) in goods_data" class="border-1px-b" :data-currentpage="current_page" :data-lastpage="last_page" :data-total="total" :data-perpage="per_page" v-if="type=='medium'">
+                <router-link :to="{name:'goods',query:{goods_id:item.goods_id,item_index:index}}"  class="block" slot="card-media">
+                  <img :src="item.images?item.images.url:'/static/grentech/default.jpg'" alt="">
                 </router-link>
-              </flexbox-item>
-            </flexbox>
-            <card-list 
-              v-for="(item,index) in goods_data" class="border-1px-b" :data-currentpage="current_page" :data-lastpage="last_page" :data-total="total" :data-perpage="per_page" v-if="type=='medium'">
-              <router-link :to="{name:'goods',query:{goods_id:item.goods_id,item_index:index}}"  class="block" slot="card-media">
-                <img :src="item.images?item.images.url:'/static/grentech/default.jpg'" alt="">
-              </router-link>
-              <router-link :to="{name:'goods',query:{goods_id:item.goods_id,item_index:index}}"  slot="card-title">
-                   <div class="item-title">{{item.name}}</div>
-                    <div class="item-title line-ellispse-2 font-bold">
-                       频段:  <span v-for="(_item,_index) in item.electrics" v-if="!!_item.workingband">{{_item.workingband}}<i v-if="(_index!=item.electrics.length-1)&&!!item.electrics[_index+1].workingband">/</i></span>
-                       <span v-if="item.electrics.length">M</span>
-                    </div>
-                    <div class="item-title line-ellispse-2 font-bold">
-                       增益: <span v-for="(_item,_index) in item.electrics" v-if="!!_item.beamgain">{{_item.beamgain}}<i v-if="(_index!=item.electrics.length-1)&&!!item.electrics[_index+1].beamgain">/</i></span>
-                       <span v-if="item.electrics.length">dBi</span>
-                    </div>
-                    <div class="item-title line-ellispse-2 font-bold">
-                       电下倾: <span v-for="(_item,_index) in item.electrics" v-if="!!_item.dipangle">{{_item.dipangle}}<i v-if="(_index!=item.electrics.length-1)&&!!item.electrics[_index+1].dipangle">/</i></span>
-                       <span v-if="item.electrics.length">°</span>
-                    </div>
-                    <div class="item-title line-ellispse-2 color-gray">
-                      SAP: {{item.bn}}
-                    </div>
-              </router-link>
-                <div class="item-subtitle color-danger padding-t-4" slot="card-subtitle">
-                  <b>¥{{item.price}}</b>
-                  <s class="padding-r-10 font-slim color-gray" v-if="item.mktprice">¥</s>
-                  <s class="color-gray font-slim">{{item.mktprice||"暂无"}}</s>
-                </div>
-            </card-list>
-          <div class="load-more text-center" v-show="loading">
-            <spinner type="circles"></spinner>
-          </div>
-          <div class="padding-tb-20 text-center" v-show="load_all" style="padding-bottom:3.3rem"><span class="iconfont">&#xe62b;</span>已加载完毕...</div>
+                <router-link :to="{name:'goods',query:{goods_id:item.goods_id,item_index:index}}"  slot="card-title">
+                    <div class="item-title">{{item.name}}</div>
+                      <div class="item-title line-ellispse-2 font-bold">
+                        频段:  <span v-for="(_item,_index) in item.electrics" v-if="!!_item.workingband">{{_item.workingband}}<i v-if="(_index!=item.electrics.length-1)&&!!item.electrics[_index+1].workingband">/</i></span>
+                        <span v-if="item.electrics.length">M</span>
+                      </div>
+                      <div class="item-title line-ellispse-2 font-bold">
+                        增益: <span v-for="(_item,_index) in item.electrics" v-if="!!_item.beamgain">{{_item.beamgain}}<i v-if="(_index!=item.electrics.length-1)&&!!item.electrics[_index+1].beamgain">/</i></span>
+                        <span v-if="item.electrics.length">dBi</span>
+                      </div>
+                      <div class="item-title line-ellispse-2 font-bold">
+                        电下倾: <span v-for="(_item,_index) in item.electrics" v-if="!!_item.dipangle">{{_item.dipangle}}<i v-if="(_index!=item.electrics.length-1)&&!!item.electrics[_index+1].dipangle">/</i></span>
+                        <span v-if="item.electrics.length">°</span>
+                      </div>
+                      <div class="item-title line-ellispse-2 color-gray">
+                        SAP: {{item.bn}}
+                      </div>
+                </router-link>
+                  <div class="item-subtitle color-danger padding-t-4" slot="card-subtitle">
+                    <b>¥{{item.price}}</b>
+                    <s class="padding-r-10 font-slim color-gray" v-if="item.mktprice">¥</s>
+                    <s class="color-gray font-slim">{{item.mktprice||"暂无"}}</s>
+                  </div>
+              </card-list>
+            <div class="load-more text-center" v-show="loading">
+              <spinner type="circles"></spinner>
+            </div>
+            <div class="padding-tb-20 text-center" v-show="load_all" style="padding-bottom:3.3rem"><span class="iconfont">&#xe62b;</span>已加载完毕...</div>
+        </div>
       </div>
-    </div>
   </div>
 </template>
   
