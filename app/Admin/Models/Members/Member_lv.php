969b789314d2377a3a790c5fd75869a30a7025ca
pengbd3 <pengbd3@163.com>
2017-04-20 14:13:23 +0800

    调整member_lv表结构

--- app/Admin/Models/Members/Member_lv.php
+++ app/Admin/Models/Members/Member_lv.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Member_lv extends Model
 {
-    protected $table ='member_lv';
+    protected $table ='member_level';
 
     /**
      * One To Many
@@ -15,6 +15,6 @@ class Member_lv extends Model
      * Member_lv.member_lv_id To Goods_lv_price.level_id
      */
     public function goods_lv_price(){
-        return $this->hasMany(Goods_lv_price::class,'level_id','member_lv_id');
+        return $this->hasMany(Goods_lv_price::class,'level_id','id');
     }
 }
